053a97bb7a6eb2eb8c3bea62b0ae727dde7b4182
Stephen Mbaalu <stephenmbaalu@gmail.com>
2023-10-10 10:02:48 +0300

    Added parse html

--- Andella/AndellaOct2023.py
+++ Andella/AndellaOct2023.py
@@ -1,26 +1,26 @@
 import configparser
 config_object = configparser.ConfigParser()
-file =open("Confi.ini","r")
-config_object.read_file(file)
-output_dict=dict()
-sections=config_object.sections()
-for section in sections:
-    output_dict[section]={}
-    items=config_object.items(section)
-    for key,val in items:
-        #print("key {}= val {}".format(key,val))
-        try:
-            output_dict[section][key] = config_object.getint(section, key)
-        except ValueError:
+with open("Confi.ini","r") as file:
+    config_object.read_file(file)
+    output_dict=dict()
+    sections=config_object.sections()
+    for section in sections:
+        output_dict[section]={}
+        items=config_object.items(section)
+        for key,val in items:
+            #print("key {}= val {}".format(key,val))
             try:
-                output_dict[section][key] = config_object.getfloat(section, key)
+                output_dict[section][key] = config_object.getint(section, key)
             except ValueError:
-                #output_dict[section][key] = config_object.get(section, key)
                 try:
-                    output_dict[section][key] = config_object.getboolean(section, key)
+                    output_dict[section][key] = config_object.getfloat(section, key)
                 except ValueError:
-                    output_dict[section][key] = config_object.get(section, key)
+                    #output_dict[section][key] = config_object.get(section, key)
+                    try:
+                        output_dict[section][key] = config_object.getboolean(section, key)
+                    except ValueError:
+                        output_dict[section][key] = config_object.get(section, key)
 
-    #output_dict[section]=dict(items)
-print("The output dictionary is:")
-print(output_dict)
+        #output_dict[section]=dict(items)
+    print("The output dictionary is:")
+    print(output_dict)
--- Andella/Pagerank.py
+++ Andella/Pagerank.py
@@ -1,7 +1,55 @@
 import os
 import networkx as nx
-
+from bs4 import BeautifulSoup
 ##in this challenge you will write a function simple_pagerank(directory) and use networkx's pagerank function to compute thepage ranking
+
+          
+def extract_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    # Extract relevant data using BeautifulSoup methods
+    # Modify this part based on the structure of the HTML on the webpage
+    title = soup.title.text if soup.title else "No Title"
+    content = soup.find('body').text.strip() if soup.find('body') else "No Content"
+    href = soup.find('a') .get("href") if soup.find('a') else ""
+    d= {'title': title, 'content': content,"href":href}
+    #print("result",d)
+    return d
+
+def simple_pagerankV2(directory):
+    # Check if the directory exists
+    if not os.path.exists(directory):
+        print(f"The directory '{directory}' does not exist.")
+        return
+
+    # Get a list of files in the directory
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+
+    # Create a directed graph using NetworkX
+    G = nx.DiGraph()
+    files_link=[]
+    for file in files:
+        with open(os.path.join(directory, file), 'r') as f:
+            links=f.read()
+            read_data=extract_data(links)
+            #print("FIle ",file," Data ",read_data)
+            G.add_node(file, **read_data)
+
+            #G.add_node(file)
+            files_link.append((file,read_data['href'],{"type": "navigation"}))
+            
+    G.add_edges_from(files_link)
+
+    page_rank_scores = nx.pagerank(G)
+
+
+    # Print or use PageRank scores and attributes for each webpage
+    for webpage, score in page_rank_scores.items():
+        attributes = G.nodes[webpage]
+        print(f"Webpage {webpage} (Score: {score}): {attributes}")
+
+    print("Result Dict ",page_rank_scores,type(page_rank_scores))
+      
+simple_pagerankV2("samplepages")
 
 def simple_pagerank(directory):
     # Check if the directory exists
@@ -21,25 +69,36 @@ def simple_pagerank(directory):
             # For simplicity, let's assume that each line in a file represents a link
             links = [line.strip() for line in f.readlines()]
             #G.add_node(file)
-            G.add_nodes_from([file])
+            
             #print(f"file:{file} link {links} " )
             G.add_edges_from(((file, link) for link in links))
-            #G.add_edges_from((file, links))
 
+            #G.add_nodes_from([file])
+           
     # Compute PageRank using NetworkX
    
-    print("Nodes================================")
-    print(G.nodes)
-    print(list(G.nodes(data=True)))
-    print("Graph================================")
+    # print("Nodes================================")
+    # print(G.nodes)
+    # print(list(G.nodes(data=True)))
+    # print("Graph================================")
+
+
     pagerank_scores = nx.pagerank(G)
+    #alpha=0.85, nstart={'A': 0, 'B': 0, 'C': 1, 'D': 0}
+
+   
+
+    #pagerank_scores2 = nx.pagerank(G, personalization={'aa.html': 0, 'bb.html': 0, 'cc.html': 0})
+
     # print("Graph================================")
     # print(G.graph)
     # print(list(G.nodes(data=True)))
     # print("Graph================================")
     # print(pagerank_scores.keys())
   
-    #print (pagerank_scores)
+    # print("pagerank_scores2 == pagerank_scores2")
+    # print (pagerank_scores2)
+    # print("pagerank_scores2 == pagerank_scores2")
 
     # Print the PageRank scores
     # print("PageRank Scores:")
@@ -51,6 +110,18 @@ def simple_pagerank(directory):
         #if ".html" in page:
         print(f"Page= {page}: Score {score}")
 
+    G.add_nodes_from(files)
+    # Create a subgraph containing only the new nodes
+    new_nodes_graph = G.subgraph(files)
+
+    # Compute PageRank for the new nodes
+    new_nodes_page_rank_scores = nx.pagerank(new_nodes_graph)
+
+    for node, score in new_nodes_page_rank_scores.items():
+        print(f"Node {node}: {score}")
+
+
+
 
     # overall_rank = sorted(pagerank_scores, key=pagerank_scores.get, reverse=True)
     # print("\nOverall Rank Based on Total PageRank Scores:")
@@ -59,53 +130,84 @@ def simple_pagerank(directory):
 
    # print("add_edges_from ",pagerank_scores[0])
     # print(dir(pagerank_scores),type(pagerank_scores))
-    sorted_nodes = sorted([(node, pagerank) for node, pagerank in pagerank_scores.items()], key=lambda x:pagerank_scores[x[0]])
-    print("Sorted Nodes:",sorted_nodes)
-    print("Added edges from")
-    for node in sorted_nodes:
-        if node[0].endswith(".html"):
-            print (node)
+    # sorted_nodes = sorted([(node, pagerank) for node, pagerank in pagerank_scores.items()], key=lambda x:pagerank_scores[x[0]])
+    # print("Sorted Nodes:",sorted_nodes)
+    # print("Added edges from")
+    # result={}
+    # for node in sorted_nodes:
+    #     if node[0].endswith(".html"):
+    #         print (node)
+    #         result[node[0]] = node[1]
+    #     elif '.html' in node[0]:
+    #         print("Node check eslse ",node[0])
+    #         for kk in result.keys():
+    #             if kk in node[0]:
+    #                 result[kk]=result[kk]+node[1]
+    #             else:
+    #                 print("Not kk in node[0] ",kk,node[0])
+    # print(result)
+
+  
 
 
-# Example usage
-simple_pagerank("samplepages")
+
+def sample_followed():
+    print(" ##New## "*7)
+
+    G = nx.DiGraph()
+
+    # Add webpages and links
+    webpages_and_links = [
+        ("home", "about"),
+        ("home", "contact"),
+        ("about", "services"),
+        ("services", "contact"),
+        ("contact", "home"),
+    ]
+
+    G.add_edges_from(webpages_and_links)
+
+    # Compute PageRank scores
+    page_rank_scores = nx.pagerank(G)
+
+    # Print or use PageRank scores for each webpage
+    for webpage, score in page_rank_scores.items():
+        print(f"Webpage {webpage}: {score}")
 
 
-def simple_pagerank2(directory):
+def other_pages():
+
     # Create a directed graph
-    graph = nx.DiGraph()
+    G = nx.DiGraph()
 
-    # Iterate over files in the specified directory
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
+    # Add webpages with attributes
+    webpages_with_attributes = {
+        "home": {"title": "Home Page", "content": "Welcome to our website."},
+        "about": {"title": "About Us", "content": "Learn more about our company."},
+        "contact": {"title": "Contact Us", "content": "Get in touch with us."},
+        "services": {"title": "Our Services", "content": "Explore our offered services."},
+    }
 
-        # Read the contents of the file and split into words
-        with open(filepath, 'r', encoding='utf-8') as file:
-            words = file.read().split()
+    G.add_nodes_from(webpages_with_attributes.items())
 
-        # Add edges to the graph based on word pairs
-        for i in range(len(words) - 1):
-            word1, word2 = words[i], words[i + 1]
-            graph.add_edge(word1, word2)
+    # Add links with attributes
+    webpage_links_with_attributes = [
+        ("home", "about", {"type": "navigation"}),
+        ("home", "contact", {"type": "navigation"}),
+        ("about", "services", {"type": "navigation"}),
+        ("services", "contact", {"type": "navigation"}),
+        ("contact", "home", {"type": "navigation"}),
+    ]
 
-    # Use networkx's pagerank function to compute pagerank values
-    pagerank_values = nx.pagerank(graph)
-    print(pagerank_values)
-    print("\n================================")
-    word_scores = {}
-    for word in pagerank_values:
-        word_scores[word] = pagerank_values[word]
+    G.add_edges_from(webpage_links_with_attributes)
 
-    return word_scores
+    # Compute PageRank scores
+    page_rank_scores = nx.pagerank(G)
 
+    # Print or use PageRank scores and attributes for each webpage
+    for webpage, score in page_rank_scores.items():
+        attributes = G.nodes[webpage]
+        print(f"Webpage {webpage} (Score: {score}): {attributes}")
 
-    r#eturn pagerank_values
-
-# Example usage
-# directory_path = 'samplepages'
-# pagerank_result = simple_pagerank2(directory_path)
-
-# # Print the PageRank values
-# print("################################ 2 ############################# ")
-# for node, score in pagerank_result.items():
-#     print(f'{node}: {score}')
+#print("other_pages()")
+#other_pages()
